cd3197c9bcb6032d8119fc4b0304d4ef93c7aaa9
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-02-03 16:49:07 +0900

    rm extra comments

--- wordwrap/wordwrap.go
+++ wordwrap/wordwrap.go
@@ -3,7 +3,6 @@ package wordwrap
 import "strings"
 
 func Wrap(s string, w int) string {
-	//return strings.ReplaceAll(s, " ", "\n")
 	if w >= len(s) {
 		return s
 	}
@@ -15,7 +14,3 @@ func Wrap(s string, w int) string {
 
 	return strings.Trim(s[:li], " ") + "\n" + Wrap(strings.Trim(s[li:], " "), w)
 }
-
-// "xx xx" -> "xx" + "xx"
-
-// "x x x" -> "x x\n" + "x"
